5772457db7d135a9b6af94bda1bfd5087fd18c80
Firuz Afzalov <afzalov98@gmail.com>
2019-12-23 04:58:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,11 @@ def introduction(name)
 end
 introduction('Firuz')
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language('Firuz, Ruby')
+
 
 
 
